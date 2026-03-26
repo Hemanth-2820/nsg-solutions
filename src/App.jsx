08b43cd68b78cc7b7cee946de01d1ce74b8788a0
hemanthsilla <hemanth.silla@oneto7solutions.in>
2026-03-26 10:52:05 +0530

    Router: Enabled dynamic/nested routing for the Services section.

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/about" element={<AboutPage />} />
-              <Route path="/services" element={<ServicesPage />} />
+              <Route path="/services/*" element={<ServicesPage />} />
               <Route path="/solutions" element={<SolutionsPage />} />
               <Route path="/contact" element={<ContactPage />} />
               <Route path="/case-studies" element={<InsightsPage />} />
